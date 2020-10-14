f13edef00eef33ff2c86ef110d21a42388a017ca
Hunter Parks <hunter@prks.cc>
2020-10-14 00:29:56 -0500

    :zap: Call backup function

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const mode = process.argv[2];
 if (mode === MODES.BACKUP) {
     logger.createLogger(path.join(process.env.ROOT_LOG_PATH, mode), 'minecraft-backup');
     const backupService = require('./service/backup.js');
-    backupService.testModule();
+    backupService.backup();
 } else {
     console.error(`Invalid mode -> ${mode}`);
 }
