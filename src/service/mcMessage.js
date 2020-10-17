df90ed7046cbb16b9e2ae9dfdf360cf86c9e2836
Hunter Parks <hunter@prks.cc>
2020-10-16 23:39:56 -0500

    :bug: Reformat tellraw command

--- src/service/mcMessage.js
+++ src/service/mcMessage.js
@@ -6,10 +6,15 @@ function players(message, hoverMessage) {
 
 function playersColor(message, hoverMessage, color) {
     if (!hoverMessage) hoverMessage = '';
+    //const commandParts = [
+    //    '\\"\\"',
+    //    `{\\"text\\":\\"[${process.env.BOT_NAME}] \\",\\"color\\":\\"gray\\",\\"italic\\":true}`,
+    //    `{\\"text\\":\\"${message}\\",\\"color\\":\\"${color}\\",\\"italic\\":true,\\"hoverEvent\\":{\\"action\\":\\"show_text\\",\\"value\\":{\\"text\\":\\"\\",\\"extra\\":[{\\"text\\":\\"${hoverMessage}\\"}]}}}`
+    //];
     const commandParts = [
-        '\\"\\"',
-        `{\\"text\\":\\"[${process.env.BOT_NAME}] \\",\\"color\\":\\"gray\\",\\"italic\\":true}`,
-        `{\\"text\\":\\"${message}\\",\\"color\\":\\"${color}\\",\\"italic\\":true,\\"hoverEvent\\":{\\"action\\":\\"show_text\\",\\"value\\":{\\"text\\":\\"\\",\\"extra\\":[{\\"text\\":\\"${hoverMessage}\\"}]}}}`
+        '""',
+        `{"text":"[${process.env.BOT_NAME}] ","color":"gray","italic":true}`,
+        `{"text":"${message}","color":"${color}","italic":true,"hoverEvent":{"action":"show_text","value":{"text":"","extra":[{"text":"${hoverMessage}"}]}}}`
     ];
     execute.mcCommand(`tellraw @a [${commandParts.join(',')}]`);
 }
