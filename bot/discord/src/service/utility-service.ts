6c02d5c95ab0791d99631fea09ebc553d9e63adf
Hunter Parks <hunter@prks.cc>
2021-01-29 22:40:31 -0600

    Remove typing

--- bot/discord/src/service/utility-service.ts
+++ bot/discord/src/service/utility-service.ts
@@ -16,59 +16,52 @@ export class UtilityService {
     }
     public static updateStatus(channel: Discord.TextChannel): void {
         if (!channel) return;
-        channel.startTyping().then(() => {
-            https.get(`${SERVICE_URL}${Config.MC_SERVER_URL}`, (response: IncomingMessage) => {
-                let data = '';
-                response.on('data', (chunk: any) => data += chunk);
-                response.on('end', () => {
-                    const res = JSON.parse(data);
-                    const embed = {
-                        color: '',
-                        title: 'Status',
-                        description: '',
-                        fields: [] as any[]
-                    };
-                    if (res.online) {
-                        embed.color = '#2f855a';
-                        embed.description = 'Online! 🥳';
-                        embed.fields.push({
-                            name: res.hostname,
-                            value: res.motd.clean[0]
-                        });
-                        embed.fields.push({
-                            name: 'Minecraft Version',
-                            value: `${res.software} ${res.version}`
-                        });
-                        embed.fields.push({
-                            name: 'Online Users',
-                            value: `${res.players.online} of ${res.players.max} online!`
-                        });
-                        if (res.players.online > 0) {
-                            const listSize = res.players.list.length;
-                            if (listSize > MAX_WIDTH) {
-                                res.players.list = res.players.list.slice(0, 2);
-                                res.players.list.push(`And ${listSize - MAX_WIDTH + 1} more`)
-                            }
-                            for (const player of res.players.list) {
-                                embed.fields.push({
-                                    name: player,
-                                    value: 'online!',
-                                    inline: true
-                                });
-                            }
+        https.get(`${SERVICE_URL}${Config.MC_SERVER_URL}`, (response: IncomingMessage) => {
+            let data = '';
+            response.on('data', (chunk: any) => data += chunk);
+            response.on('end', () => {
+                const res = JSON.parse(data);
+                const embed = {
+                    color: '',
+                    title: 'Status',
+                    description: '',
+                    fields: [] as any[]
+                };
+                if (res.online) {
+                    embed.color = '#2f855a';
+                    embed.description = 'Online! 🥳';
+                    embed.fields.push({
+                        name: res.hostname,
+                        value: res.motd.clean[0]
+                    });
+                    embed.fields.push({
+                        name: 'Minecraft Version',
+                        value: `${res.software} ${res.version}`
+                    });
+                    embed.fields.push({
+                        name: 'Online Users',
+                        value: `${res.players.online} of ${res.players.max} online!`
+                    });
+                    if (res.players.online > 0) {
+                        const listSize = res.players.list.length;
+                        if (listSize > MAX_WIDTH) {
+                            res.players.list = res.players.list.slice(0, 2);
+                            res.players.list.push(`And ${listSize - MAX_WIDTH + 1} more`)
                         }
-                    } else {
-                        embed.color = '#c53030';
-                        embed.description = 'Offline! 😭';
+                        for (const player of res.players.list) {
+                            embed.fields.push({
+                                name: player,
+                                value: 'online!',
+                                inline: true
+                            });
+                        }
                     }
-                    channel.bulkDelete(100, true)
-                        .then(() => {
-                            channel.send({ embed })
-                                .then(() => {
-                                    channel.stopTyping();
-                                });
-                        });
-                });
+                } else {
+                    embed.color = '#c53030';
+                    embed.description = 'Offline! 😭';
+                }
+                channel.bulkDelete(100, true)
+                    .then(() => channel.send({ embed }));
             });
         });
     }
